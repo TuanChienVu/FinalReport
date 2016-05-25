4d332b1bd8f9325c05bd229035f3eb6be3554360
Hoàng Đồng <hoangdongbmt@gmail.com>
2016-05-25 12:37:35 +0700

    dong moi

---
+++ app/src/main/java/com/dclover/gpsutilities/khoihanh/speed/IBaseGpsListener.java
@@ -0,0 +1,23 @@
+package com.dclover.gpsutilities.khoihanh.speed;
+
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+/**
+ * Created by Kinghero on 5/25/2016.
+ */
+public interface IBaseGpsListener extends LocationListener, GpsStatus.Listener {
+
+    public void onLocationChanged(Location location);
+
+    public void onProviderDisabled(String provider);
+
+    public void onProviderEnabled(String provider);
+
+    public void onStatusChanged(String provider, int status, Bundle extras);
+
+    public void onGpsStatusChanged(int event);
+
+}
