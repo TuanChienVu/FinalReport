c71b014459410993a1ae1ff0375dd87845298b03
Hoàng Đồng <hoangdongbmt@gmail.com>
2016-07-02 12:29:38 +0700

    ok

--- app/src/main/java/com/dclover/gpsutilities/login/Login.java
+++ app/src/main/java/com/dclover/gpsutilities/login/Login.java
@@ -1,16 +1,19 @@
 package com.dclover.gpsutilities.login;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
+import com.dclover.gpsutilities.MainActivity;
 import com.dclover.gpsutilities.R;
-import com.dclover.gpsutilities.taxi.Activity.LoginActivity;
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -29,20 +32,26 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        String linkBD = getBaseContext().getResources().getString(R.string.linkDB);
         Firebase.setAndroidContext(this);
         final Firebase myFirebaseRef = new Firebase(linkBD);
         llLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myFirebaseRef.authWithPassword(edEmailLogin.getText().toString(), edPassLogin.getText().toString(),
+                myFirebaseRef.authWithPassword(editEmail.getText().toString(), edtPass.getText().toString(),
                         new Firebase.AuthResultHandler() {
                             @Override
                             public void onAuthenticated(AuthData authData) {
-                                Toast.makeText(Login.this, "User ID: " + authData.getUid() +
-                                        ", Provider: " + authData.getProvider(), Toast.LENGTH_SHORT).show();
-                                SharedPrefsUtils.setStringPreference(LoginActivity.this,"UserID",authData.getUid());
-                                SharedPrefsUtils.setStringPreference(LoginActivity.this,"UserName",edEmailLogin.getText().toString());
-                                Intent intent = new Intent(Login.this, ChatActivity.class);
+
+                                new AsyncTask<Void,Void,Void>()
+                                {
+
+                                    @Override
+                                    protected Void doInBackground(Void... params) {
+                                        return null;
+                                    }
+                                }.execute();
+                                Intent intent = new Intent(Login.this, MainActivity.class);
                                 startActivity(intent);
 
                             }
@@ -50,7 +59,7 @@ public class Login extends AppCompatActivity {
                             @Override
                             public void onAuthenticationError(FirebaseError firebaseError) {
                                 // there was an error
-                               Log.d("login.error", firebaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                               Log.d("login.error", firebaseError.getMessage());
                             }
                         });
 
